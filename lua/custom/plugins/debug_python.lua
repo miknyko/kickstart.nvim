daf620218789846f6b11af0c2572a0a42646f7e5
rockstarr <41538890+miknyko@users.noreply.github.com>
2024-03-25 22:47:53 +0800

    add python debugg

---
+++ lua/custom/plugins/debug_python.lua
@@ -0,0 +1,15 @@
+return {
+    "mfussenegger/nvim-dap-python",
+
+    ft = "python",
+    dependencies = {
+        "mfussenegger/nvim-dap",
+    },
+
+
+    -- change this to debug python interpreter path
+    config = function(_, opts)
+        local path = "~/miniconda3/envs/py311/bin/python"
+        require("dap-python").setup(path)
+    end,
+}
